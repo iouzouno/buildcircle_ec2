d72d6c54efcdede33beb55bf237eae9da9eee0b6
iouzouno <89134918+iouzouno@users.noreply.github.com>
2025-10-01 18:51:28 +0300

    Initial commit of buildcircle_ec2 project

--- ec2.tf
+++ ec2.tf
@@ -4,7 +4,7 @@ resource "aws_instance" "terraform-instance" {
   ami                    = var.AMIS[var.REGION]
   instance_type          = "t2.micro"
   subnet_id              = aws_subnet.public_subnet_1.id
-  key_name               = aws_key_pair.my_key.key_name
+  #key_name               = aws_key_pair.my_key.key_name
   vpc_security_group_ids = [aws_security_group.terrafrom_sg.id]
   user_data              = file("userdata.tpl")
   tags = {
@@ -12,10 +12,10 @@ resource "aws_instance" "terraform-instance" {
   }
 }
 
-resource "aws_key_pair" "my_key" {
-  key_name   = "my-key" # this is what EC2 will reference
-  public_key = var.PUB_KEY
-}
+#resource "aws_key_pair" "my_key" {
+#  key_name   = "my-key" # this is what EC2 will reference
+#  public_key = var.PUB_KEY
+#}
 
 resource "aws_eip" "my_ip" {
 
